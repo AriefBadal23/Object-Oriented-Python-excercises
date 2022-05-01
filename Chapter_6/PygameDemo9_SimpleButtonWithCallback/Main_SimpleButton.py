361b52dc56673ea225892de409f66d3ab12725a0
Arief Badal <1042170@hr.nl>
2022-05-01 12:30:55 +0200

    Fix bug of excercise 7 of Chapter 6

--- Chapter_6/PygameDemo9_SimpleButtonWithCallback/Main_SimpleButton.py
+++ Chapter_6/PygameDemo9_SimpleButtonWithCallback/Main_SimpleButton.py
@@ -1,98 +1,99 @@
-# pygame demo 9 -3-button test witj callbacks
+# pygame demo 4(a) - one image, move by keyboard
+""" Small programs that allows to move the ball image using the keyboard """
 
-# 1 - Import packages
+# Import packages
 import pygame
 from pygame.locals import *
-from SimpleButton import *
 import sys
+import random
+from SimpleButton import *
 
+# Define constants
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 400
+FRAMERS_PER_SECOND = 30
 
-# 2 - Define constants
-GRAY = (200, 200, 200) 
-WINDOW_WIDTH = 400
-WINDOW_HEIGHT = 100
-FRAMES_PER_SECOND = 60
 
 # Define a function to be used as a "callback"
-def my_call_back_function():
-    print('User pressed Button B, called myCallBackfunction')
+def myCallBackFunction():
+    print('User Pressed B, called my myCallBackFunction')
 
 
-
-# Define a classs with a method to be useed as a "callback"
-class Call_back_test():
+# Define a class with a method to be used as a "callback"
+class CallBackTest():
     def __init__(self):
         pass
+    
+    def myMethod(self):
+        print('User pressed Button C, called myMethod of the CallBackTest object')
 
-    def my_method(self):
-        print('User pressed Button C, called my_method of the call_back_test object')
 
-
-
-# 3 - Initialize the world
+# Initialize the world
 pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
- 
-# 4 - Load assets: image(s), sound(s),  etc.
 
-# 5 - Initialize variables
+# 4 Load assets: image(s), sounds, etc
 
-oCallBackTest = Call_back_test()
-# Create instances of SimpleButton
-# No call back
+
+# 5 Initialize the variables
+oCallBackTest = CallBackTest()
+
+
 oButtonA = SimpleButton(window, (25, 30),
-                        'images/buttonAUp.png',
-                        'images/buttonADown.png')
+                            'images/buttonAUp.png',
+                            'images/buttonADown.png')
 
-#Specifying a function to call back 
 oButtonB = SimpleButton(window, (150, 30),
-                        'images/buttonBUp.png',
-                        'images/buttonBDown.png',
-                        call_back=my_call_back_function)
+                            'images/buttonBUp.png',
+                            'images/buttonBDown.png',
+                            call_back=myCallBackFunction)
 
-# Specifying method to call back
 oButtonC = SimpleButton(window, (275, 30),
-                        'images/buttonCUp.png',
-                        'images/buttonCDown.png',
-                        call_back=oCallBackTest.my_method)
+                            'images/buttonCUp.png',
+                            'images/buttonCDown.png',
+                            call_back=oCallBackTest.myMethod)
 
 counter = 0
 
-# 6 - Loop forever
-while True:
 
-     # 7 - Check for and handle events
+# Loop forever
+while True:
+    # Check for and handle events:
     for event in pygame.event.get():
         # Clicked the close button? Quit pygame and end program
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
-        # Pass the event to the button, see if it has been clicked on
+        
+        # Pass the event to the button, see if has been clicked on
         if oButtonA.handleEvent(event):
-            print("User has clicked the button A, handled in the main loop")
+            print('User pressed button A, handled in the main loop')
 
-            # oButtonB and ObuttonC hadve callbacks,
-            # no need to check result of these calss
-            oButtonB.handleEvent(event)
+        # oButtonB and OButtonC have callbacks,
+        # no need to check result of these calls
+        oButtonB.handleEvent(event)
+        
+        oButtonC.handleEvent(event)
 
-            oButtonC.handleEvent(event)
 
-    # 8 - Do any "per frame" actions
-    counter= counter + 1
 
-    # 9 - Clear the window
-    window.fill(GRAY)
+    # 9 Do any 'per frame' actions
+    counter = counter + 1
 
-    # 10 - Draw all window elements
+
+    # Clear the window before drawing it again
+    window.fill(BLACK)
+
+    # 10 Draw the window elements
     oButtonA.draw()
     oButtonB.draw()
     oButtonC.draw()
 
-
-    # 11 - Update the window
+    # 11 Update the window
     pygame.display.update()
 
-    # 12 - Slow things down a bit
-    clock.tick(FRAMES_PER_SECOND)  # make pygame wait
+    # 12 Slow thing down a bit
+    clock.tick(FRAMERS_PER_SECOND)
