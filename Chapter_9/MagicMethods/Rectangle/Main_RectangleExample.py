e18e1b214ea03fbcac59d0e65681c9306824bc5b
Arief Badal <1042170@hr.nl>
2022-06-03 13:48:00 +0200

    Add excercise 7 of Chapter 9

--- Chapter_9/MagicMethods/Rectangle/Main_RectangleExample.py
+++ Chapter_9/MagicMethods/Rectangle/Main_RectangleExample.py
@@ -1,3 +1,4 @@
+from shutil import which
 import pygame
 import sys
 from pygame.locals import *
@@ -13,54 +14,48 @@ N_RECTANGLES = 10
 FIRST_RECTANGLE = 'first'
 SECOND_RECTANGLE = 'second'
 
-
-
-#Set up the window 
 pygame.init()
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 0, 32)
+window  = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 0, 32)
 clock = pygame.time.Clock()
 
+
 rectangle_list = []
-for item in range(0, N_RECTANGLES):
-    oRectangle = Rectangle(window)
-    rectangle_list.append(oRectangle)
+for i in range(0, N_RECTANGLES):
+    ORectangle = Rectangle(window)
+    rectangle_list.append(ORectangle)
 
 which_rectangle = FIRST_RECTANGLE
 
-# Main loop
+
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
-
         if event.type == MOUSEBUTTONDOWN:
-            for oRectangle in rectangle_list:
-                if oRectangle.clicked_inside(event.pos):
-                    print('Clicked on', which_rectangle, 'rectangle')
+            if ORectangle in rectangle_list:
+                if ORectangle in rectangle_list:
+                    if ORectangle.clicked_inside(event.pos):
+                        print('Clicked on', which_rectangle, 'rectangle')
 
-                if which_rectangle == FIRST_RECTANGLE:
-                    ofirst_rectangle = oRectangle
-                    which_rectangle = SECOND_RECTANGLE
+                        if which_rectangle == FIRST_RECTANGLE:
+                            oFirstRectangle = ORectangle
+                            which_rectangle = SECOND_RECTANGLE
 
-                elif which_rectangle == SECOND_RECTANGLE:
-                    osecond_rectangle = oRectangle
-                    # User has chosen 2 rectangles lets compare
-                    if ofirst_rectangle == osecond_rectangle:
-                        print('Rectangles are the same size')
+                        elif which_rectangle == SECOND_RECTANGLE:
+                            oSecondRectangle = ORectangle
+                            oSecondRectangle < ORectangle
+                            print('First rectangle is smaller than second rectangle.')
+                        else:
+                             print('First rectangle is larger than second rectangle.')
+                        whichRectangle = FIRST_RECTANGLE
 
-                    elif ofirst_rectangle < osecond_rectangle:
-                        print('First rectangle is smaller than second rectangle')
-
-                    else: #must be larger
-                        print('First rectangle is larger than scond rectangle')
-                    which_rectangle = FIRST_RECTANGLE
-
-    # Clear the window and draw all rectangles
+    # Clear the window an draw all rectangless
     window.fill(WHITE)
     for oRectangle in rectangle_list:
-        oRectangle.draw()
+        ORectangle.draw()
 
-    pygame.display.update()
+    pygame.display.update
     clock.tick(FRAMES_PER_SECOND)
--- Chapter_9/MagicMethods/Rectangle/Rectangle.py
+++ Chapter_9/MagicMethods/Rectangle/Rectangle.py
@@ -1,72 +1,65 @@
-# Rectlangle class
+
 import pygame
 import random
 
-# Setup the colors
-RED = (255,000)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
+# Set up the colors
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
 
 class Rectangle():
+
     def __init__(self, window):
         self.window = window
-        self.height = random.choice((20, 30, 40))
         self.width = random.choice((20, 30, 40))
-        self.color = ((RED, GREEN, BLUE))
+        self.height = random.choice((20, 30, 40))
+        self.color = random.choice((RED, GREEN, BLUE))
         self.x = random.randrange(0, 400)
         self.y = random.randrange(0, 400)
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         self.area = self.width * self.height
 
-        def clicked_inside(self, mouse_point):
-            clicked = self.rect.collidepoint(mouse_point)
-            return clicked
+    def clicked_inside(self, mousePoint):
+        clicked = self.rect.collidepoint(mousePoint)
+        return clicked
 
-        # Magic methods called when you compare
-        # Two rectangle objects with the == operator
+    # Magic method called when you compare
+    # two Rectangle  objects with the == operator
+    def __eq__(self, oOtherRectangle):
+        if not isinstance(oOtherRectangle, Rectangle):
+            raise TypeError('Second object was not a Rectangle')
+        if self.area == oOtherRectangle.area:
+            return True
+        else:
+            return False
 
-        def __eq__(self, oOther_rectangle, rectangle):
-            if self.area == oOther_rectangle.area:
-                return True
-            else:
-                return False
+    # Magic method called when you compare
+    # two Rectangle  objects with the < operator
+    def __lt__(self, oOtherRectangle):
+        if not isinstance(oOtherRectangle, Rectangle):
+            raise TypeError('Second object was not a Rectangle')
+        if self.area < oOtherRectangle.area:
+            return True
+        else:
+            return False
 
-        
-        # Magic method called when you compare two rectangle objects with the
-        # < operator
+    # Magic method called when you compare
+    # two Rectangle  objects with the > operator
+    def __gt__(self, oOtherRectangle):
+        if not isinstance(oOtherRectangle, Rectangle):
+            raise TypeError('Second object was not a Rectangle')
+        if self.area > oOtherRectangle.area:
+            return True
+        else:
+            return False
 
-        def __lt__(self, oOther_rectangle):
-            if not isinstance(oOther_rectangle, Rectangle):
-                raise TypeError("Second object was not an a Rectangle")
-            if self.area < oOther_rectangle:
-                return True
+    def getArea(self):
+        return self.area
 
-            else: 
-                return False
-
-
-        # Magic method called when you compare two rect. objects with the > operator
-        def __gt__(self, oOther_rectangle):
-            if not isinstance(oOther_rectangle, Rectangle):
-                raise TypeError('Second object was not a Rectangle')
-
-            if self.area > oOther_rectangle.area:
-                return True
-            
-            else:
-                return False
-
-
-
-        def get_area(self):
-            return self.area
-
-        
-        def draw(self):
-            pygame.draw.rect(self.window, self.color, (self.x, self.y, self.width, self.height))
-
-
-
+    def draw(self):
+        pygame.draw.rect(self.window, self.color, (self.x, self.y,
+                         self.width, self.height))
 
 # Magic methods
 # client code
