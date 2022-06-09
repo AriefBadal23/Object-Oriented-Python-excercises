fdc809daad366bc0d39261782d5926df8f2eceef
Arief Badal <1042170@hr.nl>
2022-06-09 21:28:55 +0200

    Fix bug increment accountnumber by each new bank instance

--- OOP_Excercises/Bank.py
+++ OOP_Excercises/Bank.py
@@ -1,17 +1,17 @@
 from Account import *
 
 class Bank():
+    next_account_number = 0
     def __init__(self):
         self.accountdict = {}
-        self.next_account_number = 0
+        Bank.next_account_number += 1
 
 
     def create_account(self, name, start_balance, password):
         oAccount = Account(name, start_balance, password)
         new_account_number = self.next_account_number
         self.accountdict[new_account_number] = oAccount
-        self.next_account_number = self.next_account_number + 1
-        return new_account_number   
+        return new_account_number
 
 
     def open_account(self):
@@ -19,27 +19,20 @@ class Bank():
         print('**Open a account**')
         username = input('What is your name? ')
         start_balance = input('What is the start balance? ')
+        start_balance = int(start_balance)
         password = input('What is the password that you want to use? ')
         user_account_number = self.create_account(username, start_balance, password)
-        print(f'Your new account number is:{user_account_number}')
+        print('Your new account number is:', user_account_number)
 
     def close_account(self):
         pass
 
 
 
-p = Bank()
-print(p.create_account('Arief', 400, 'password123'))
+Person1 = Bank()
 
-p2 = Bank()
-p2.create_account('John', 2000, 'password123')
+print(Person1.open_account())
 
-p3 = Bank()
-p3.create_account('Marieke', 2000, 'password123')
+Person2 = Bank()
 
-
-
-        
-
-
-    
+print(Person2.open_account())
