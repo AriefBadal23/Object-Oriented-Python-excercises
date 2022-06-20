b3f029da5bcd9bfe050b85e98a50f861e094ebbc
Arief Badal <1042170@hr.nl>
2022-06-21 00:11:21 +0200

    Add excercise 2 of Chapter 10

--- Chapter_10/EmployeeManagerInheritance/EmployeeManagerInheritance.py
+++ Chapter_10/EmployeeManagerInheritance/EmployeeManagerInheritance.py
@@ -22,5 +22,46 @@ class Employee():
         pay = 52 * 5 * 8 * self.rate_per_hour
         return pay
 
+class Manager(Employee):
+    def __init__(self, name, title, salary, reports_list=None):
+        self.salary = float(salary)
+        if reports_list is None:
+            reports_list = []
+        self.report_list = reports_list
+        super().__init__(name, title)
 
-        
+    def get_reports(self):
+        return self.report_list
+
+    def pay_per_year(self, give_bonus=False):
+        pay= self.salary
+        if give_bonus:
+            pay= pay + (10 * self.salary) # add a bonus of 10%
+            print(self.name, 'gets a bonus for good work')
+            return pay
+
+
+# create objects
+oEmployee1 = Employee('Joe Schmoe', 'Pizza Maker', 16)
+oEmployee2 = Employee('Chris Smith', 'Cashier', 14)
+oManager = Manager('Sue Jones', 'Pizza Restaurant Manager', 5500, [oEmployee1, oEmployee2])
+
+# Call methods of the Employee objects
+print('Employee name: ', oEmployee1.get_name())
+print('Employee salary: ', '{:,.2f}'.format(oEmployee1.pay_per_year()))
+print('Employee name: ', oEmployee2.get_name())
+print('Employee salary: ', '{:,.2f}'.format(oEmployee2.pay_per_year()))
+print()
+
+# Call the methods of the Manager object
+manager_name = oManager.get_name()
+print('Manager name:', manager_name)
+
+# Give the manager a bonus
+print('Manager salary: ', '{:,.2f}'.format(oManager.pay_per_year(True)))
+print(manager_name, '(' + oManager.get_title() + ')', 'direct reports:')
+reports_list = oManager.get_reports()
+for oEmployee in reports_list:
+    print(' ', oEmployee.get_name(),
+    '(' + oEmployee.get_title() + ')')
+
